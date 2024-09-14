4f58ca5cecf24cafeadd3030380d2eaf168ff1d4
Psanti-gif <psantixt12@gmail.com>
2024-09-13 20:15:18 -0500

    Primeros Cambios

---
+++ Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double sueldo,valor_v;
+        int ventas;
+        double comision = 10;
+
+        Console.Write("Ingrese su Sueldo Base: $");
+        sueldo=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese Cantidad de ventas: ");
+        ventas = Convert.ToInt32(Console.ReadLine());
+
+    }
+}
