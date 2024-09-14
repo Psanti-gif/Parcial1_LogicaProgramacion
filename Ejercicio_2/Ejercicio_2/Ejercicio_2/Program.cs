8a9af0fa482a1e78b8db0ea537e1fe2e9a7b4c57
Psanti-gif <psantixt12@gmail.com>
2024-09-13 22:29:23 -0500

    Finalizado Completo

--- Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
+++ Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿/*
+ * Escriba un programa que pida los coeficientes de una ecuación de primer grado (a x + b = 0) y muestre la solución.
+Una ecuación de primer grado puede no tener solución, tener una solución única, o que todos los números sean solución. La fórmula de las soluciones es x = -b / a
+ */
 class Program
 {
     static void Main()
