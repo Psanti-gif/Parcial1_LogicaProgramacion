ca3199464b2334bf96a3ce8699358506d2c2af6a
Psanti-gif <psantixt12@gmail.com>
2024-09-13 22:15:51 -0500

    Ejercicio Finalizado

--- Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
+++ Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
@@ -2,14 +2,29 @@
 class Program
 {
     static void Main()
-    {
-        // Pedir los coeficientes a y b al usuario
+    { 
         Console.Write("Introduce el coeficiente a: ");
         double a = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Introduce el coeficiente b: ");
         double b = Convert.ToDouble(Console.ReadLine());
 
-        
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Todos los números son solución.");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no tiene solución.");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"La solución es: x = {x}");
+        }
+
     }
 }
