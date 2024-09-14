833538fb25ef5fac93a7a86dbc8ccc7523cbaa8d
Psanti-gif <psantixt12@gmail.com>
2024-09-13 20:23:20 -0500

    Cambios 2

--- Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
@@ -4,6 +4,7 @@
     {
         double sueldo,valor_v;
         int ventas;
+        double beneficio = 0;
         double comision = 10;
 
         Console.Write("Ingrese su Sueldo Base: $");
@@ -11,6 +12,15 @@
 
         Console.Write("Ingrese Cantidad de ventas: ");
         ventas = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de la venta: $");
+        valor_v = Convert.ToDouble(Console.ReadLine());
+
+        if (ventas==3 && valor_v >= 1000000)
+        {
+            beneficio = 100000;
+            Console.Write($"Ganaste el Beneficio:{beneficio:C} ");
+        }
 
     }
 }
