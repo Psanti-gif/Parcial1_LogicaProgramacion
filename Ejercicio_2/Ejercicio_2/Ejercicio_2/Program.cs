f62fb99d57e09a77e63d0b2b9ea9eb2c3bf15060
Psanti-gif <psantixt12@gmail.com>
2024-09-13 22:12:41 -0500

    Primeros Cambios

---
+++ Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // Pedir los coeficientes a y b al usuario
+        Console.Write("Introduce el coeficiente a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduce el coeficiente b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        
+    }
+}
