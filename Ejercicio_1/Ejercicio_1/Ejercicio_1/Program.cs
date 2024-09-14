09aa11e346e95992e48695d3efc174bfb6d52f59
Psanti-gif <psantixt12@gmail.com>
2024-09-13 20:54:08 -0500

    Cambios 3

--- Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
@@ -2,8 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        double sueldo,valor_v;
-        int ventas;
+        double sueldo,valor_v,valor_TotalV;
+        int Numventas;
         double beneficio = 0;
         double comision = 10;
 
@@ -11,16 +11,29 @@
         sueldo=Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Ingrese Cantidad de ventas: ");
-        ventas = Convert.ToInt32(Console.ReadLine());
+        Numventas = Convert.ToInt32(Console.ReadLine());
+
+
+        if (Numventas < 0)
+        {
+            Console.Write("La cantidad de ventas no puede ser negativa, ingrese nuevamente las ventas realizadas: ");
+            Numventas = Convert.ToInt32(Console.ReadLine());
+
+        }
+        else if (Numventas > 3)
+        {
+            Console.Write("El Máximo de ventas posibles a realizar para acceder al beneficio es de 3");
+            beneficio = 100000;
+        }
+        else if (Numventas == 3)
+        {
+
+            beneficio = 100000;
+            Console.Write($"Por la cantidad de ventas realizadas la empresa te dara un beneficio de: ${beneficio}");
+        }
 
         Console.Write("Ingrese el valor de la venta: $");
         valor_v = Convert.ToDouble(Console.ReadLine());
-
-        if (ventas==3 && valor_v >= 1000000)
-        {
-            beneficio = 100000;
-            Console.Write($"Ganaste el Beneficio:{beneficio:C} ");
-        }
 
     }
 }
